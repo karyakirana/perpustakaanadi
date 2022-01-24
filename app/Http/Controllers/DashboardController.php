14fc13834efd3de6f9cc33ae2f644fb2acd88361
karyakirana <adibpratama2@gmail.com>
2022-01-25 03:23:21 +0700

    authorization

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends Controller
     public function index()
     {
         // dashboard admin
-        if (Auth::user()->role != 'pegawai' | Auth::user()->role != 'admin'){
+        if (Auth::user()->role != 'pegawai'){
             return redirect()->to('dashboard/'.Auth::user()->role);
         }
         return view('pages.dashboard');
